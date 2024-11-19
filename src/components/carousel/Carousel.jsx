cdd26d15c91ed35750a7c3ebe061ea83b99ab481
arunsharmalearning <arunlivingfantastico@gmail.com>
2024-11-20 02:12:50 +0530

    fix carousel bug

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -14,30 +14,29 @@ const ControlledCarousel = ({ data }) => {
 
   return (
     <Carousel className="mainPage_carousel" interval={5000} pause={false} indicators={false}>
-      {data && data.map((item) => {
-        (
-          <Carousel.Item key={item.id} >
-            <img
-              loading="lazy"
-              className="d-block w-100"
-              src={`${API_URL}${item.image}`}
-              style={{ height: "100%", filter: "brightness(75%)" }}
-              alt="Pre Slide"
-            />
+      {data && data.map((item) =>
+      (
+        <Carousel.Item key={item.id} >
+          <img
+            loading="lazy"
+            className="d-block w-100"
+            src={`${API_URL}${item.image}`}
+            style={{ height: "100%", filter: "brightness(75%)" }}
+            alt="Pre Slide"
+          />
 
-            <Carousel.Caption className="carousel_caption">
-              <h3 className="float-animation">
-                {item.title}
-              </h3>
-              <div className="dropdown">
-                <Stack direction={"row"} className="">
-                  <StyledButtonLink href={"/donate-now"}>{"Donate Now"}</StyledButtonLink>
-                </Stack>
-              </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-        )
-      }
+          <Carousel.Caption className="carousel_caption">
+            <h3 className="float-animation">
+              {item.title}
+            </h3>
+            <div className="dropdown">
+              <Stack direction={"row"} className="">
+                <StyledButtonLink href={"/donate-now"}>{"Donate Now"}</StyledButtonLink>
+              </Stack>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+      )
       )}
     </Carousel >
   );
